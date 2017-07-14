96ad2026f185170dc2ed4368347811c287be55f8
Misuzu <471215158@qq.com>
2017-07-14 17:06:45 +0800

    Medusa

--- app/src/main/java/com/cloud4magic/freecast/ui/VersionActivity.java
+++ app/src/main/java/com/cloud4magic/freecast/ui/VersionActivity.java
@@ -1,26 +1,40 @@
 package com.cloud4magic.freecast.ui;
 
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.TextView;
 
 import com.cloud4magic.freecast.MyAplication;
 import com.cloud4magic.freecast.R;
 import com.cloud4magic.freecast.utils.Logger;
+import com.cloud4magic.freecast.utils.RetrofitHelper;
 import com.cloud4magic.freecast.utils.ToastUtil;
 import com.cloud4magic.freecast.widget.LoadingDialogFragment;
 import com.demo.sdk.Scanner;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetAddress;
 import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import okhttp3.ResponseBody;
+import rx.Observable;
+import rx.Subscriber;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
 
 /**
  * 软硬件版本
@@ -96,6 +110,7 @@ public class VersionActivity extends AppCompatActivity {
                             isInitDevice = true;
                             mDeviceIdText.setText(mDeviceId);
                             mDeviceIpText.setText(mDeviceIp);
+                            mLoadingDialogFragment.dismiss();
                             break;
                         }
                     }
@@ -138,11 +153,129 @@ public class VersionActivity extends AppCompatActivity {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
                                 dialog.cancel();
+                                launchAppDetail(getPackageName(),"");
                             }
                         })
+                        .setCancelable(false)
                         .create()
                         .show();
                 break;
         }
     }
+
+    /**
+     * 启动到应用商店app详情界面
+     *
+     * @param appPkg    目标App的包名
+     * @param marketPkg 应用商店包名 ,如果为""则由系统弹出应用商店列表供用户选择,否则调转到目标市场的应用详情界面，某些应用商店可能会失败
+     */
+    public void launchAppDetail(String appPkg, String marketPkg) {
+        try {
+            if (TextUtils.isEmpty(appPkg)) return;
+
+            Uri uri = Uri.parse("market://details?id=" + appPkg);
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            if (!TextUtils.isEmpty(marketPkg)) {
+                intent.setPackage(marketPkg);
+            }
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 获取下载链接 并下载文件
+     */
+    private void getDownLoadLink()
+    {
+        RetrofitHelper.getInstance()
+                .getService()
+                .getDownloadLink("http://www.cv-hd.com/upgrade.txt")
+                .flatMap(new Func1<ResponseBody, Observable<ResponseBody>>() {
+                    @Override
+                    public Observable<ResponseBody> call(ResponseBody responseBody) {
+                        try {
+                            String[] array = responseBody.string().split("\n");
+                            return RetrofitHelper.getInstance().getService().downloadFile(array[1]);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        return Observable.empty();
+                    }
+                })
+                .subscribeOn(Schedulers.io())
+                .subscribe(new Subscriber<ResponseBody>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onNext(ResponseBody responseBody) {
+                        writeResponseBodyToDisk(responseBody);
+                    }
+                });
+    }
+
+
+    /**
+     * 写入文件到本地
+     */
+    private boolean writeResponseBodyToDisk(ResponseBody body) {
+        try {
+            File upgradeFile = new File(getExternalFilesDir(null) + File.separator + "upgrade.tar");
+
+            InputStream inputStream = null;
+            OutputStream outputStream = null;
+
+            try {
+                byte[] fileReader = new byte[4096];
+
+                long fileSize = body.contentLength();
+                long fileSizeDownloaded = 0;
+
+                inputStream = body.byteStream();
+                outputStream = new FileOutputStream(upgradeFile);
+
+                while (true) {
+                    int read = inputStream.read(fileReader);
+
+                    if (read == -1) {
+                        break;
+                    }
+
+                    outputStream.write(fileReader, 0, read);
+
+                    fileSizeDownloaded += read;
+
+                    Logger.e("Misuzu", "file download: " + fileSizeDownloaded + " of " + fileSize+upgradeFile.getAbsolutePath());
+                }
+
+                outputStream.flush();
+
+                return true;
+            } catch (IOException e) {
+                return false;
+            } finally {
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+
+                if (outputStream != null) {
+                    outputStream.close();
+                }
+            }
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+
 }
