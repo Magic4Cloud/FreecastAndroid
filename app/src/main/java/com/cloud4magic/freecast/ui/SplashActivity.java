2b37c0a0b299825b82864a84ce9ca3357809301a
xmzd <xmzd92@163.com>
2017-07-24 17:06:36 +0800

    启动页增加loading效果

--- app/src/main/java/com/cloud4magic/freecast/ui/SplashActivity.java
+++ app/src/main/java/com/cloud4magic/freecast/ui/SplashActivity.java
@@ -3,11 +3,20 @@ package com.cloud4magic.freecast.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
+import android.widget.ProgressBar;
 
 import com.cloud4magic.freecast.MainActivity;
+import com.cloud4magic.freecast.R;
+
+import java.lang.ref.SoftReference;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * 启动页
@@ -15,32 +24,105 @@ import com.cloud4magic.freecast.MainActivity;
  * Editor  Misuzu
  */
 
-public class SplashActivity extends AppCompatActivity{
+public class SplashActivity extends AppCompatActivity {
 
-    Handler mHandler;
-    Runnable mRunnable;
+    private Unbinder mUnbinder;
+    @BindView(R.id.splash_progress)
+    ProgressBar mProgressBar;
+    private MyHandler mHandler;
+    private int mProgress = 0;
+    private float mTimeDelta;
+
+    private static final int UPDATE_PROGRESS = 100;
+
+    private static class MyHandler extends Handler {
+
+        private SoftReference<SplashActivity> mSoftReference;
+
+        MyHandler(SplashActivity activity) {
+            mSoftReference = new SoftReference<SplashActivity>(activity);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            SplashActivity activity = mSoftReference.get();
+            if (activity == null) {
+                return;
+            }
+            switch (msg.what) {
+                case UPDATE_PROGRESS:
+                    activity.mProgressBar.setProgress(activity.mProgress);
+                    break;
+            }
+        }
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-//        ImageView imageView = new ImageView(this);
-//        imageView.setImageResource(R.drawable.temp_splash_img);
-//        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-//        setContentView(imageView);
-        mHandler = new Handler();
-        mHandler.postDelayed(mRunnable = new Runnable() {
+        setContentView(R.layout.activity_splash);
+        mUnbinder = ButterKnife.bind(this);
+        // update progress
+        updateProgress();
+        mHandler = new MyHandler(this);
+        mHandler.postDelayed(mRunnable, 2000);
+    }
+
+    /**
+     * Runnable, to MainActivity
+     */
+    private Runnable mRunnable = new Runnable() {
+        @Override
+        public void run() {
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            finish();
+        }
+    };
+
+    /**
+     * update ProgressBar
+     */
+    private void updateProgress() {
+        mProgressBar.setMax(100);
+        mProgressBar.setProgress(0);
+        final long startTime = System.currentTimeMillis();
+        mTimeDelta = System.currentTimeMillis() - startTime;
+        new Thread(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashActivity.this, MainActivity.class));
-                finish();
+                while (mTimeDelta <= 1600) {
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    mProgress = (int) (mTimeDelta / 1600f * 100f);
+                    if (mHandler != null) {
+                        mHandler.sendEmptyMessage(UPDATE_PROGRESS);
+                    }
+                    mTimeDelta = System.currentTimeMillis() - startTime;
+                }
+                mProgress = 100;
+                if (mHandler != null) {
+                    mHandler.sendEmptyMessage(UPDATE_PROGRESS);
+                }
             }
-        },2000);
+        }).start();
     }
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
         mHandler.removeCallbacks(mRunnable);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mUnbinder != null) {
+            mUnbinder.unbind();
+        }
     }
 }
